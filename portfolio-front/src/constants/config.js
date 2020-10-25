c8321a096cdf43fc3b5a4f5bfdc6736b5141e87e
simranjj <simranjeet271@gmail.com>
2020-10-24 23:22:55 -0400

    Adding Constants and Routes

--- portfolio-front/src/constants/config.js
+++ portfolio-front/src/constants/config.js
@@ -1,3 +1,4 @@
 export const PORTFOLIO_SERVER99 = "https://portfolioserver99.herokuapp.com/users";
 export const DEDICATED_LOGIN_USERNAME = "simran";
 export const DEDICATED_LOGIN_PASSWORD = "simran";
+export const TEMPLATE_ID = 'template_bpboggh';
--- portfolio-front/src/routes/index.js
+++ portfolio-front/src/routes/index.js
@@ -1,20 +1,21 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Login from '../component/Login';
+import Login from '../component/profile/login/Login';
 import Profile from '../component/profile/Profile';
-import SignUp from '../component/SignUp';
+import SignUp from '../component/profile/signup/SignUp';
 import EditProfile from '../component/edit_profile/EditProfile';
-import DedicatedLogin from '../component/profile/dedicated_login/DedicatedLogin'
+import DedicatedLogin from '../component/profile/dedicated_login/DedicatedLogin';
 
 const Routes = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter forceRefresh={true}>
     <Switch>
     <Route exact path="/" component={DedicatedLogin} />
     <Route exact path="/login" component={Login} />
     <Route exact path="/signup" component={SignUp} />
     <Route exact path="/profile" component={Profile} />
     <Route exact path="/editprofile" component={EditProfile} />
+    <Route component={Profile} />
     </Switch>
     </BrowserRouter>
   );
